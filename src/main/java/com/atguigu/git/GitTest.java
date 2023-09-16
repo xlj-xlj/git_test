7c9b15eb3bdd1a469735f3b2f4ae6ff69a4c9031
git_x <2899673172@qq.com>
2023-09-16 09:02:54 +0800

    push_commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,6 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello git_hot_fix!");
+        System.out.println("hello push_test");
 
     }
 
